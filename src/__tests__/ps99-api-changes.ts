f2d43dc88058b1f70d4e5ccd8f0b160d266e32a9
Joseph Lawson <joe@joekiller.com>
2024-06-16 00:52:29 -0400

    test: leave out live tests

--- src/__tests__/ps99-api-changes.ts
+++ src/__tests__/ps99-api-changes.ts
@@ -5,32 +5,32 @@ import { PetSimulator99API } from "../ps99-api";
 /**
  * It's hard to snapshot these because certain values will always be changing but useful for dump-result checks
  */
-describe("Pet Simulator Public Live API Test - Changing Items", () => {
-  const api = new PetSimulator99API();
-
-  test('Active Clan Battle', async () => {
-    const results = await api.getActiveClanBattle();
-    expect(results).toMatchSnapshot();
-  });
-
-  test('Get Clan', async () => {
-    const results = await api.getClan('MMFC');
-    expect(results).toMatchSnapshot();
-  });
-
-  test('Get Clans', async () => {
-    const results = await api.getClans({pageSize: 1});
-    expect(results).toMatchSnapshot();
-  });
-
-  test('Get Exists', async () => {
-    const results = await api.getExists();
-    expect(results).toMatchSnapshot();
-  });
-
-  test('Get RAP', async () => {
-    const results = await api.getRAP();
-    expect(results).toMatchSnapshot();
-  });
-
-});
+// describe("Pet Simulator Public Live API Test - Changing Items", () => {
+//   const api = new PetSimulator99API();
+//
+//   test('Active Clan Battle', async () => {
+//     const results = await api.getActiveClanBattle();
+//     expect(results).toMatchSnapshot();
+//   });
+//
+//   test('Get Clan', async () => {
+//     const results = await api.getClan('MMFC');
+//     expect(results).toMatchSnapshot();
+//   });
+//
+//   test('Get Clans', async () => {
+//     const results = await api.getClans({pageSize: 1});
+//     expect(results).toMatchSnapshot();
+//   });
+//
+//   test('Get Exists', async () => {
+//     const results = await api.getExists();
+//     expect(results).toMatchSnapshot();
+//   });
+//
+//   test('Get RAP', async () => {
+//     const results = await api.getRAP();
+//     expect(results).toMatchSnapshot();
+//   });
+//
+// });
