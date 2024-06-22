b31a415ceb36b4b6c50cb159ad19cf98a42a619d
Joseph Lawson <joe@joekiller.com>
2024-06-22 02:48:04 -0400

    feat: update misc-item type

--- src/responses/collection/misc-item.ts
+++ src/responses/collection/misc-item.ts
@@ -8,15 +8,25 @@ export type MiscItemData = {
   hash: any;
 };
 
-export type MiscItemConfigData = {
-  DisplayName: string;
-  GetOffers: any;
-  MachineName: string;
-  PriceMult: number;
-  RefreshRate: number;
-  SlotRespectLevels?: number[];
-  StockRangeByRespectLevel?: number[][];
-  HideNotification?: boolean;
-  HideRespect?: boolean;
-  IsStatic?: boolean;
-};
+export interface MiscItemConfigData {
+  Rarity: MiscItem
+  DisplayName: string
+  Category: string
+  Icon: string
+  Desc: string
+  Tradable?: boolean
+  AltIcon?: string
+}
+
+export interface MiscItem {
+  RarityNumber: number
+  Lootbag: any
+  _id: string
+  Color: any
+  DisplayName: string
+  Message: any
+  ItemSlot: any
+  Gradient: any
+  Announce: boolean
+  _script: any
+}
