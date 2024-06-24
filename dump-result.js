fd716d067ab0b856ffcc2c3b1cf891072fae47f0
Joseph Lawson <joe@joekiller.com>
2024-06-24 00:43:44 -0400

    refactor: update dump-result to just dump all the collections

--- dump-result.js
+++ dump-result.js
@@ -2,7 +2,20 @@
  * convert a snapshot result to friendly json and then update interface via
  * https://transform.tools/json-to-typescript
  */
-let dumpResult = [];
 
-const fs = require("fs");
-fs.writeFileSync("out.json", JSON.stringify(dumpResult, null, 2));
+const { PetSimulator99API } = require("./dist/ps99-api");
+const api = new PetSimulator99API();
+api.getCollections().then(async (response) => {
+  if (response.status === "ok") {
+    const collections = response.data;
+    for await (const [c, collection] of collections.map((c) =>
+      api.getCollection(c).then((_c) => [c, _c]),
+    )) {
+      const fs = require("fs");
+      fs.writeFileSync(
+        `dist/out-${c}.json`,
+        JSON.stringify(collection, null, 2),
+      );
+    }
+  }
+});
